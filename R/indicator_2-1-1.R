6c66d46eccfdaa034324803554e67679d0a71620
ZiQi <ziqi2011@gmail.com>
2024-06-13 12:29:55 -0400

    Added reporting for 2.1.1

---
+++ R/indicator_2-1-1.R
@@ -0,0 +1,32 @@
+# Indicator 2.1.1 ---------------------------------------------------------
+# 2.1.1 Prevalence of undernourishment
+
+options(timeout = 300) 
+library(cansim)
+library(tidyr)
+library(dplyr)
+library(stringr)
+library(devtools)
+install_github("expersso/OECD")
+library(OECD)
+
+primary_url <- "https://nsi-release-ro-statsuite.fao.org/rest/data/FAO,DF_SN_ITK_DEFC_211,1.0/A...124........?startPeriod=2015&dimensionAtObservation=AllDimensions"
+FAO_Primary <- readsdmx::read_sdmx(primary_url)
+
+data_final <-
+  drop_na(FAO_Primary) %>%
+  select(
+    Year = TIME_PERIOD,
+    Units = UNIT_MEASURE,
+    Value = ObsValue
+  ) %>% 
+  mutate(
+    Units = "Percentage (%)"
+  )
+
+write.csv(
+  data_final,
+  "data/indicator_2-1-1.csv",
+  row.names = FALSE,
+  na = ""
+)
