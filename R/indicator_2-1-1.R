dcdc7517074ef64da2496d4f759f1f0f814e0f02
tristanmenard <tristan.menard@outlook.com>
2024-10-02 11:37:28 -0400

    cleanup and UTF-8 encoding

--- R/indicator_2-1-1.R
+++ R/indicator_2-1-1.R
@@ -1,17 +1,13 @@
 # Indicator 2.1.1 ---------------------------------------------------------
 # 2.1.1 Prevalence of undernourishment
 
-options(timeout = 300) 
-library(cansim)
 library(tidyr)
 library(dplyr)
 library(stringr)
-library(devtools)
-install_github("expersso/OECD")
-library(OECD)
+library(readsdmx)
 
 primary_url <- "https://nsi-release-ro-statsuite.fao.org/rest/data/FAO,DF_SN_ITK_DEFC_211,1.0/A...124........?startPeriod=2015&dimensionAtObservation=AllDimensions"
-FAO_Primary <- readsdmx::read_sdmx(primary_url)
+FAO_Primary <- read_sdmx(primary_url)
 
 data_final <-
   drop_na(FAO_Primary) %>%
@@ -23,11 +19,13 @@ data_final <-
   mutate(
     Units = "Percentage (%)",
     Value = str_remove(Value, '<')
-  ) 
+  ) %>%
+  mutate_at("Value", as.numeric)
   
 write.csv(
   data_final,
   "data/indicator_2-1-1.csv",
   row.names = FALSE,
-  na = ""
+  na = "",
+  fileEncoding = "UTF-8",
 )
