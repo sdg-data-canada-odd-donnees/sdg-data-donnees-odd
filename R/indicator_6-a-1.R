fb2f8d1be23038ef42d251ddc416c6d7ddb62f52
tristanmenard <tristan.menard@outlook.com>
2024-11-07 10:26:49 -0500

    update API call and re-calculate from OECD table

--- R/indicator_6-a-1.R
+++ R/indicator_6-a-1.R
@@ -1,5 +1,5 @@
 # Indicator 6.a.1 ---------------------------------------------------------
-# 6.a.1 Amount of water- and sanitation-related official development 
+# Amount of water- and sanitation-related official development 
 # assistance that is part of a government-coordinated spending plan
 
 options(timeout = 300)
@@ -7,9 +7,9 @@ library(dplyr)
 library(readsdmx)
 
 # CRS: Creditor Reporting System (flows)
-# See https://data-explorer.oecd.org/vis?pg=0&bp=true&snb=19&vw=tb&df[ds]=dsDisseminateFinalDMZ&df[id]=DSD_CRS%40DF_CRS&df[ag]=OECD.DCD.FSD&df[vs]=1.1&dq=DAC..1000.100._T._T.D.Q._T..&lom=LASTNPERIODS&lo=5&to[TIME_PERIOD]=false&lc=en
-# Applied filters: Donor = Canada, Recipient = Developing countries, Sector = Water supply & sanitation/Agricultural water resources, Measure = Official Development Assistance, Price base = Constant prices
-url_oda <- "https://sdmx.oecd.org/dcd-public/rest/data/OECD.DCD.FSD,DSD_CRS@DF_CRS,1.1/DAC..31140+140.100._T._T.D.Q._T..?startPeriod=2015&dimensionAtObservation=AllDimensions"
+# See https://data-explorer.oecd.org/vis?fs[0]=Topic%2C1%7CDevelopment%23DEV%23%7COfficial%20Development%20Assistance%20%28ODA%29%23DEV_ODA%23&pg=0&fc=Topic&bp=true&snb=19&vw=tb&df[ds]=dsDisseminateFinalDMZ&df[id]=DSD_CRS%40DF_CRS&df[ag]=OECD.DCD.FSD&df[vs]=1.2&dq=CAN.DPGC.140%2B31140.100._T._T.D.Q._T..&pd=2015%2C&to[TIME_PERIOD]=false
+# Applied filters: Donor = Canada, Recipient = Developing countries, Sector = Water supply & sanitation/Agricultural water resources, Measure = Official Development Assistance, Flow type = Disbursements, Price base = Constant prices
+url_oda <- "https://sdmx.oecd.org/dcd-public/rest/data/OECD.DCD.FSD,DSD_CRS@DF_CRS,1.2/CAN.DPGC.140+31140.100._T._T.D.Q._T..?startPeriod=2015&dimensionAtObservation=AllDimensions"
 
 oda_raw <- read_sdmx(url_oda)
 
