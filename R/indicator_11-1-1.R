04997d554e95431e09548801a6c5bb38ff533d3a
tristanmenard <tristan.menard@outlook.com>
2024-11-20 16:05:09 -0500

    update R script to new CODR and run it 11.1.1

--- R/indicator_11-1-1.R
+++ R/indicator_11-1-1.R
@@ -1,16 +1,14 @@
 # 11.1.1 ------------------------------------------------------------------
-
-# Large urban population centres have a population of 100,000 or more.
+# Proportion of urban population living in slums, informal settlements or inadequate housing
 
 library(cansim)
 library(dplyr)
 library(stringr)
 
-core_housing <- get_cansim("46-10-0065-01", factors = FALSE)
+raw_data <- get_cansim("46-10-0085-01", factors = FALSE)
 geocodes <- read.csv("geocodes.csv")
 
-core_housing <- 
-  core_housing %>%
+core_housing <- raw_data %>%
   filter(
     `Core housing need statistics` == "Percentage of persons in core housing need",
     str_detect(GEO, "urban")
@@ -20,33 +18,21 @@ core_housing <-
     Geography = GEO,
     Tenure = `Tenure including first-time homebuyer and social and affordable housing status`,
     Value = VALUE
-  ) %>% 
+  ) %>%
   mutate(
-    Geography = str_remove(Geography, "Large urban population centres, ")
-  ) %>% 
+    Geography = str_remove(Geography, "Large urban population centres, "),
+    Geography = replace(Geography, Geography == "Total, large urban population centres", "Canada (provinces only)"),
+    # Set headline
+    across(
+      c(Geography, Tenure),
+      ~ replace(., Geography == "Canada (provinces only)" & Tenure == "Total, tenure", NA)
+    )
+  ) %>%
   left_join(geocodes, by = "Geography") %>% 
   relocate(GeoCode, .before = "Value")
 
-total_line <- 
-  core_housing %>% 
-  filter(
-    Geography == "Total, large urban population centres",
-    Tenure == "Total, tenure"
-  ) %>% 
-  mutate(across(2:3, ~ ""))
-
-data_final <-
-  bind_rows(
-    total_line,
-    core_housing %>%
-      filter(!(
-        Geography == "Total, large urban population centres" &
-          Tenure == "Total, tenure"
-      ))
-  )
-
 write.csv(
-  data_final,
+  core_housing,
   "data/indicator_11-1-1.csv",
   na = "",
   row.names = FALSE,
