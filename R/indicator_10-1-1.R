63eb3b03aee5e65d4afefe0f33a78299e0d0dd04
tristanmenard <tristan.menard@outlook.com>
2024-12-20 13:00:39 -0500

    automatic calculation of growth rate over the given period, add geography disaggregation, separate columns for decile and quintile categories

--- R/indicator_10-1-1.R
+++ R/indicator_10-1-1.R
@@ -1,4 +1,6 @@
-# 10.1.1 ------------------------------------------------------------------
+# Indicator 10.1.1 -------------------------------------------------------------
+# Growth rates of household expenditure or income per capita among the bottom
+# 40 per cent of the population and the total population
 
 library(dplyr)
 library(cansim)
@@ -6,7 +8,7 @@ library(tidyr)
 
 exp <- get_cansim("11-10-0223-01", factors = FALSE)
 income <- get_cansim("11-10-0193-01", factors = FALSE)
-pop <- get_cansim("17-10-0005-01", factors = FALSE)
+pop_raw <- get_cansim("17-10-0005-01", factors = FALSE)
 
 quintiles <- c(
   "All quintiles",
@@ -24,51 +26,61 @@ deciles <- c(
 
 cagr <- function(present, past, n) ((present/past)^(1/n)) - 1
 
-cagr_data_transform <- function(data) {
-  
-  data %>% 
-    left_join(pop) %>% 
-    transmute(
+cagr_data_transform <- function(data, pop, period) {
+  # Inner join population with data and calculate per capita values 
+  data <- data %>%
+    inner_join(pop) %>% 
+    mutate(
+      Year = as.numeric(Year),
+      Value = Value/Population
+    ) %>%
+    select(
       Year,
       Geography,
-      `Household income quintile or decile`,
-      Value = Value/Population
-    ) %>% 
-    tidyr::pivot_wider(
-      names_from = "Year",
-      values_from = "Value"
-    ) %>%
-    transmute(
-      Geography,
-      `Household income quintile or decile`,
-      `2010-2015` = cagr(`2015`, `2010`, 5),
-      `2011-2016` = cagr(`2016`, `2011`, 5),
-      `2012-2017` = cagr(`2017`, `2012`, 5),
-      `2014-2019` = cagr(`2019`, `2014`, 5)
-    ) %>% 
-    tidyr::pivot_longer(
-      cols = 3:6,
-      names_to = "Year",
-      values_to = "Value"
-    ) %>% 
-    relocate(Year) %>% 
-    mutate(
-      Value = round(Value*100, 3)
+      any_of(c("Household income quintile", "Household income decile")),
+      Value
     )
+  # Get the earliest and latest years in the data
+  yearmin <- min(data$Year)
+  yearmax <- max(data$Year)
+  # Get a list of missing years in the data between the earliest and latest years
+  years_missing <- setdiff(yearmin:yearmax, unique(data$Year))
+  # Pivot table so that each year becomes a separate column
+  data <- tidyr::pivot_wider(data, names_from = "Year", values_from = "Value")
+  # Add columns with NA values for any years that are missing
+  if (length(years_missing) > 0) {
+    for (yi in years_missing) {
+      data[[as.name(yi)]] <- NA
+    }
+  }
+  # Calculate the compound annual growth rate for each year over the given period
+  # Add the calculated growth rate values as new columns
+  for (yi in yearmin:(yearmax - period)){
+    newcolname <- paste(yi, yi + period, sep = "-") # e.g. "2015-2020"
+    data[[newcolname]] <- cagr(data[[as.name(yi + period)]], data[[as.name(yi)]], period)
+  }
+  
+  data <- data %>%
+    # Remove the columns for per capita values for each year
+    select(-any_of(as.character(yearmin:yearmax))) %>%
+    # Pivot table back so that growth rate values for each year are in separate rows
+    gather(key = "Year", value = "Value", -Geography, -any_of(c("Household income quintile", "Household income decile"))) %>%
+    relocate(Year) %>%
+    mutate(Value = round(Value*100, 3))
   
 }
 
-pop <- 
-  pop %>% 
+pop <- pop_raw %>% 
   filter(
     REF_DATE >= 2010,
-    GEO == "Canada",
+    # GEO == "Canada",
     Gender == "Total - gender",
     `Age group` == "All ages"
   ) %>% 
   select(
     Year = REF_DATE,
-    Population = VALUE
+    Geography = GEO,
+    Population = VALUE,
   )
 
 
@@ -77,7 +89,7 @@ pop <-
 expenditure_per_cap <-
   exp %>% 
   filter(
-    GEO == "Canada",
+    # GEO == "Canada",
     Statistic == "Average expenditure per household",
     `Before-tax household income quintile` %in% quintiles,
     `Household expenditures, summary-level categories` == "Total current consumption"
@@ -85,10 +97,10 @@ expenditure_per_cap <-
   select(
     Year = REF_DATE,
     Geography = GEO,
-    `Household income quintile or decile` = `Before-tax household income quintile`,
+    `Household income quintile` = `Before-tax household income quintile`,
     Value = VALUE
   ) %>%
-  cagr_data_transform() %>% 
+  cagr_data_transform(pop, 5) %>% 
   mutate(
     Series = "Household expenditure per capita"
   )
@@ -108,10 +120,10 @@ income_per_cap <-
   select(
     Year = REF_DATE,
     Geography = GEO,
-    `Household income quintile or decile` = `Income decile`,
+    `Household income decile` = `Income decile`,
     Value = VALUE
   ) %>% 
- cagr_data_transform() %>% 
+ cagr_data_transform(pop, 5) %>% 
  mutate(
     Series = "Income per capita"
   )
@@ -119,7 +131,8 @@ income_per_cap <-
 data_final <- 
   bind_rows(expenditure_per_cap, income_per_cap) %>% 
   relocate(Series, .after = Year) %>% 
-  select(-Geography) # temp
+  relocate(`Household income decile`, .after = `Household income quintile`) %>%
+  relocate(Geography, .after = `Household income decile`)
 
 write.csv(
   data_final,
