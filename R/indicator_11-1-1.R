898e7eda391e547ceab22e3ce8905387081220f6
ZiQi <ziqi2011@gmail.com>
2025-02-20 13:23:35 -0500

    Enhance data processing for indicator 11-1-1: add tidyr library, update Geography handling, and remove specific entries

--- R/indicator_11-1-1.R
+++ R/indicator_11-1-1.R
@@ -4,6 +4,7 @@
 library(cansim)
 library(dplyr)
 library(stringr)
+library(tidyr)
 
 raw_data <- get_cansim("46-10-0085-01", factors = FALSE)
 geocodes <- read.csv("geocodes.csv")
@@ -15,18 +16,24 @@ core_housing <- raw_data %>%
   ) %>% 
   select(
     Year = REF_DATE,
-    Geography = GEO,
     Tenure = `Tenure including first-time homebuyer and social and affordable housing status`,
+    Geography = GEO,
     Value = VALUE
   ) %>%
   mutate(
     Geography = str_remove(Geography, "Large urban population centres, "),
+    Geography = case_when(
+      Geography == "Total, large urban population centres" ~ "Canada",
+      TRUE ~ Geography
+    )) %>%
+  mutate(
     # Set headline
     across(
       c(Geography, Tenure),
-      ~ replace(., Geography == "Total, large urban population centres" & Tenure == "Total, tenure", NA)
+      ~ replace(., Geography == "Canada" & Tenure == "Total, tenure", NA)
     )
   ) %>%
+  drop_na(Value) %>%
   left_join(geocodes, by = "Geography") %>% 
   relocate(GeoCode, .before = "Value")
 
