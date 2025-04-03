c82612b2aaca77a5384cbb09c62b684f458f60da
ZiQi <ziqi2011@gmail.com>
2025-04-03 11:45:26 -0400

    Refactor indicator 17-19-1 data retrieval to use CSV format and streamline data processing

--- R/indicator_17-19-1.R
+++ R/indicator_17-19-1.R
@@ -12,41 +12,14 @@ library(httr)
 # Sector = Statistical capacity building
 # Flow type = Disbursements, Price base = Constant prices
 
-url <- "https://sxs-boost-oecd.redpelicans.com/boost-disseminate/v2/sdmx/data/OECD.DCD.FSD,DSD_CRS@DF_CRS,1.3/CAN.DPGC.16062.100._T._T.D.Q._T..?startPeriod=2015&dimensionAtObservation=AllDimensions"
-response <- GET(url)
-
-if (status_code(response) == 200) {
-  stat_CRS_json <- content(response)
-  # Process the data as needed
-} else {
-  print(paste("Error:", status_code(response)))
-}
-
-# Extract observation
-statistical <- stat_CRS_json$data$dataSets[[1]]$observations
-
-# Convert observations to a more manageable format
-# Extract the values and their corresponding keys
-obs_keys <- names(statistical)
-obs_values <- lapply(statistical, function(x) x[[1]])  # Access the first element in the list
-
-# Create a data frame
-statistical_df <- data.frame(
-  Key = obs_keys,
-  Value = sapply(obs_values, function(x) x[1])  # Extract the first value from the c() vector
-)
-
-# Convert Value to numeric
-statistical_df$Value <- as.numeric(statistical_df$Value)
-
-# Add a Year column starting from 2015
-statistical_df$Year <- 2015 + seq(0, nrow(statistical_df) - 1)
+url <- "https://sxs-boost-oecd.redpelicans.com/boost-disseminate/v2/sdmx/data/OECD.DCD.FSD,DSD_CRS@DF_CRS,1.3/CAN.DPGC.16062.100._T._T.D.Q._T..?startPeriod=2015&dimensionAtObservation=AllDimensions&format=csvfile"
+statistics <- read.csv(url)
 
 data_final <-
-  statistical_df %>%
+  statistics %>%
   select(
-    Year,
-    Value
+    Year = TIME_PERIOD,
+    Value = OBS_VALUE
   )
 
 write.csv(
