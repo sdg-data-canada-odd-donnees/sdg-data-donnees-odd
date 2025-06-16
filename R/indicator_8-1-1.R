b93bef9014244084724969be46921c4d3df021cd
tristanmenard <tristan.menard@outlook.com>
2025-06-16 16:32:38 -0400

    simplify 8.1.1 script and add progress column

--- R/indicator_8-1-1.R
+++ R/indicator_8-1-1.R
@@ -9,52 +9,51 @@ real_gdp <- get_cansim("36-10-0222-01", factors = FALSE)
 pop_ests <- get_cansim("17-10-0005-01", factors = FALSE)
 geocodes <- read.csv("geocodes.csv")
 
-national_gdp <- real_gdp %>% 
+national_gdp <- real_gdp %>%
   filter(
     substr(REF_DATE, 1, 4) >= 2014,
     Estimates == "Gross domestic product at market prices",
     Prices == "Chained (2017) dollars"
-  ) %>% 
-  select(
-    REF_DATE,
-    Geography = GEO,
-    VALUE
-  ) %>% 
+  ) %>%
   mutate(
     Year = substr(REF_DATE, 1, 4)
-  ) %>% 
-  group_by(Year, Geography) %>% 
-  summarise(GDP = mean(VALUE), .groups = "drop") %>%
+  ) %>%
+  select(
+    Year,
+    Geography = GEO,
+    GDP = VALUE
+  ) %>%
+  arrange(Year, Geography) %>%
   na.omit()
 
-pop_ests <- pop_ests %>% 
+pop_ests_filtered <- pop_ests %>%
   filter(
     REF_DATE >= 2014,
     Gender == "Total - gender",
     `Age group` == "All ages",
     UOM == "Persons"
-  ) %>% 
+  ) %>%
   select(
     Year = REF_DATE,
     Geography = GEO,
     Population = VALUE
   )
 
-all_gdp <- national_gdp %>% 
-  inner_join(pop_ests) %>% 
+all_gdp <- national_gdp %>%
+  inner_join(pop_ests_filtered) %>%
   mutate(
-    gdp_per_cap = (GDP*1000000)/Population
-  ) %>% 
-  arrange(Geography, Year) %>% 
-  group_by(Geography) %>% 
+    gdp_per_cap = (GDP * 1000000) / Population
+  ) %>%
+  arrange(Geography, Year) %>%
+  group_by(Geography) %>%
   mutate(
     Geography = recode(Geography, Canada = ""),
-    Value = (gdp_per_cap - lag(gdp_per_cap)) / lag(gdp_per_cap),
-    Value = round(Value * 100, 2)
-  ) %>% 
-  filter(Year > 2014) %>% 
-  select(-c(3:5)) %>% 
-  left_join(geocodes) %>% 
+    Progress = round(gdp_per_cap, 2),
+    Value = round(((gdp_per_cap - lag(gdp_per_cap)) / lag(gdp_per_cap)) * 100, 2),
+  ) %>%
+  filter(Year > 2014) %>%
+  select(-c(3:5)) %>%
+  left_join(geocodes) %>%
   relocate(GeoCode, .before = "Value")
 
 write.csv(
@@ -64,4 +63,3 @@ write.csv(
   row.names = FALSE,
   fileEncoding = "UTF-8"
 )
-
